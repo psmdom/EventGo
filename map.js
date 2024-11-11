f0bd56f3bf1aed5dbbf521584e0b4d7742293bbf
Martin Anderson <mander24@students.cs.ubc.ca>
2024-11-10 16:01:11 -0800

    Created functions for creating markers and descriptions for those markers. Used these to make some sample markers

--- map.js
+++ map.js
@@ -1,15 +1,3 @@
-// New Variables
-var titles = "Study Session";
-var timings = "Every Tuesday at 3 PM";
-var locations = "Room 101, Library";
-
-var RealContent = `
-  <strong>${titles}</strong><br>
-  <em>Timing: </em> ${timings} <br>
-  <em>Location: </em> ${locations}
-`;
-
-
 // initialize the map on the "map" div with a given center and zoom
 var map = L.map('map', {
     center: [49.26036, -123.24961],
@@ -25,12 +13,12 @@ function makeIcon(url) {
     })
 }
 
-const iconChilling = makeIcon('static/images/map-icons/chilling.png')
-const iconExploring = makeIcon('static/images/map-icons/exploring.png')
-const iconExercising = makeIcon('static/images/map-icons/exercising.png')
-const iconSocializing = makeIcon('static/images/map-icons/socializing.png')
-const iconStudying = makeIcon('static/images/map-icons/studying.png')
-const currentIcon = iconExercising
+const iconChilling = makeIcon('static/images/map-icons/chilling.png');
+const iconExploring = makeIcon('static/images/map-icons/exploring.png');
+const iconExercising = makeIcon('static/images/map-icons/exercising.png');
+const iconSocializing = makeIcon('static/images/map-icons/socializing.png');
+const iconStudying = makeIcon('static/images/map-icons/studying.png');
+const currentIcon = iconStudying;
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -38,6 +26,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([49.26036, -123.24961], {icon: currentIcon}).addTo(map);
-marker.bindPopup(RealContent)
-// var marker = L.marker([49.26036, -123.24561]).addTo(map);
+
+// Marker functions
+function makeMarker(longitude, latitude, icon, description) {
+    var marker = L.marker([longitude, latitude], {icon: icon}).addTo(map);
+    marker.bindPopup(description);
+}
+
+function makeDescription(title, timing, location) {
+    return `
+        <strong>${title}</strong><br>
+        <em>Timing: </em> ${timing} <br>
+        <em>Location: </em> ${location}
+    `
+}
+
+
+// Sample descriptions:
+var studyDescription = makeDescription("Study Session", "Every Tuesday at 3:00 p.m.", "IBLC, Room 192");
+var exerciseDescription = makeDescription("Spike Ball", "Every Friday at 5:20 p.m.", "MacInnes Field");
+
+// Sample markers:
+makeMarker(49.2674646,-123.2525383, iconStudying, studyDescription);
+makeMarker(49.266877, -123.249152, iconExercising, exerciseDescription);
