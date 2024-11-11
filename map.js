8ce617b9e4f643f99f159c26ec27b8f89157ddf4
Martin Anderson <mander24@students.cs.ubc.ca>
2024-11-10 18:39:16 -0800

    Added locations and categories and refactored the map code to make use of these

--- map.js
+++ map.js
@@ -1,10 +1,4 @@
-// initialize the map on the "map" div with a given center and zoom
-var map = L.map('map', {
-    center: [49.26036, -123.24961],
-    zoom: 14
-});
-
-function makeIcon(url) {
+function constrcutIconObject(url) {
     return L.icon({
         iconUrl: url,
         iconSize: [35, 50],
@@ -13,39 +7,122 @@ function makeIcon(url) {
     })
 }
 
-const iconChilling = makeIcon('static/images/map-icons/chilling.png');
-const iconExploring = makeIcon('static/images/map-icons/exploring.png');
-const iconExercising = makeIcon('static/images/map-icons/exercising.png');
-const iconSocializing = makeIcon('static/images/map-icons/socializing.png');
-const iconStudying = makeIcon('static/images/map-icons/studying.png');
-const currentIcon = iconStudying;
+const Categories = Object.freeze({
+    CHILLING: {icon: constrcutIconObject('static/images/map-icons/chilling.png'), list: [], visible: true},
+    EXERCISING: {icon: constrcutIconObject('static/images/map-icons/exercising.png'), list: [], visible: true},
+    EXPLORING: {icon: constrcutIconObject('static/images/map-icons/exploring.png'), list: [], visible: true},
+    SOCIALIZING: {icon: constrcutIconObject('static/images/map-icons/socializing.png'), list: [], visible: true},
+    STUDYING: {icon: constrcutIconObject('static/images/map-icons/studying.png'), list: [], visible: true}
+});
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    minZoom: 14,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+const Locations = Object.freeze({   
+    ALLARD: { position: [49.270013, -123.253314] },
+    SAUDER: { position: [49.26514, -123.253885] },
+    MEDICAL_SCHOOL: { position: [49.262505, -123.245224] },
+    BEATY_BIODIVERSITY: { position: [49.26323, -123.250977] },
+    MUSEUM_OF_ANTHROPOLOGY: { position: [49.269473, 123.25946] },
+    EARTHQUAKE_ENGINEERING: { position: [49.262814, -123.251974] },
+    DAVID_STRANGWAY: { position: [49.2658998,-123.2485868] },
+    BIOMEDICAL_RESEARCH: { position: [ 49.2640731,-123.2470508] },
+    TOTEM_PARK: { position: [ 49.2580136,-123.2555449] },
+    PLACE_VANIER: { position: [49.2646816,-123.2611698] },
+    GAGE_TOWERS: { position: [49.269691,-123.2518995] },
+    MARINE_DRIVE: { position: [49.2615961,-123.2585585] },
+    FAIRVIEW_CRESCENT: { position: [49.2633125,-123.2427608] },
+    IKB_LIBRARY: { position: [49.2676038,-123.2527386] },
+    WOODWARD_LIBRARY: { position: [49.2644947,-123.249834] },
+    LAW_LIBRARY: { position: [49.2697988,-123.2561645] },
+    CHAN_CENTRE: { position: [49.2698,-123.255851] },
+    FREDERIC_WOOD: { position: [49.2683065,-123.257307] },
+    BELKIN_ART_GALLERY: { position: [49.268245,-123.256301] },
+    THUNDERBIRD_SPORTS_CENTRE: { position: [49.2600575,-123.2416822] },
+    AQUATIC_CENTRE: { position: [49.2677587,-123.2487584] },
+    WAR_MEMORIAL_GYM: { position: [49.2666872,-123.2477647] },
+    LIFE_BUILDING: { position: [49.2676248,-123.2501682] },
+    BOOKSTORE: { position: [49.2652012,-123.2503841] },
+    HEALTH_SERVICES: { position: [49.2641259,-123.2457641] },
+    OLD_AUDITORIUM: { position: [49.2658096,-123.2589044] },
+    CLOCK_TOWER: { position: [49.267337,-123.2545625] },
+    ROSE_GARDEN: { position: [49.2693791,-123.2564979] },
+    BOTANICAL_GARDEN: { position: [49.2541042,-123.2507623] },
+    TRIUMF: { position: [49.2477653,-123.2309023] },
+    AMS_STUDENT_NEST: { position: [49.266612,-123.2500257] },
+    WRECK_BEACH: { position: [49.260187,-123.2665235] },
+    KOERNER: {position: [49.2666555,-123.2552087]},
+    MACINNES_FIELD: {position: [49.2668708, -123.2491213]},
+    UBC: {position: [49.26036, -123.24961]}
+});
 
+// function updateMap() {
+    // initialize the map on the "map" div with a given center and zoom
+    var map = L.map('map', {
+        center: [49.26036, -123.24961],
+        zoom: 14
+    });
 
-// Marker functions
-function makeMarker(longitude, latitude, icon, description) {
-    var marker = L.marker([longitude, latitude], {icon: icon}).addTo(map);
-    marker.bindPopup(description);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        minZoom: 14,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // var markersToDisplay = [];
+
+    // if (Categories.CHILLING.visible) {
+    //     markersToDisplay.push(CHILLING.list)
+    // } else if (Categories.EXERCISING.visible) {
+    //     markersToDisplay.push(EXERCISING.list)
+    // } else if (Categories.EXPLORING.visible) {
+    //     markersToDisplay.push(EXPLORING.list)
+    // } else if (Categories.SOCIALIZING.visible) {
+    //     markersToDisplay.push(SOCIALIZING.list)
+    // } else if (Categories.STUDYING.visible) {
+    //     markersToDisplay.push(STUDYING.list)
+    // } 
+
+    // markersToDisplay.forEach(element => {
+    //     element.forEach(element => {
+    //         formattedDescription = formatDescription(element.title, element.time, element.place, element.description);
+    //         makeMarker(element.location.position, element.category.icon, formatDescription)
+    //     })
+    // });
+// }
+
+// // initialize for the first time
+// updateMap();
+
+function makeEvent(category, location, title, time, place, description) {
+    category.list.push({
+        category: category,
+        location: location,
+        title: title,
+        time: time,
+        place: place,
+        description: description
+    });
+    
+    formattedDescription = formatDescription(title, time, place, description);
+    makeMarker(location.position, category.icon, formattedDescription);
 }
 
-function makeDescription(title, timing, location) {
+function formatDescription(title, time, location, description) {
     return `
         <strong>${title}</strong><br>
-        <em>Timing: </em> ${timing} <br>
-        <em>Location: </em> ${location}
+        <em>Time: </em> ${time} <br>
+        <em>Location: </em> ${location} <br>
+        <em>Description: </em> ${description}
     `
 }
 
+function makeMarker(position, icon, formattedDescription) {
+    var marker = L.marker(position, {icon: icon}).addTo(map);
+    marker.bindPopup(formattedDescription);
+}
 
 // Sample descriptions:
-var studyDescription = makeDescription("Study Session", "Every Tuesday at 3:00 p.m.", "IBLC, Room 192");
-var exerciseDescription = makeDescription("Spike Ball", "Every Friday at 5:20 p.m.", "MacInnes Field");
+var studyDescription = formatDescription("Study Session", "Every Tuesday at 3:00 p.m.", "IBLC, Room 192", "Come become an academic weapon with SUS.");
+var exerciseDescription = formatDescription("Spikeball", "Every Friday at 5:20 p.m.", "MacInnes Field", "Come play competitive or casual spikeball.");
 
 // Sample markers:
-makeMarker(49.2674646,-123.2525383, iconStudying, studyDescription);
-makeMarker(49.266877, -123.249152, iconExercising, exerciseDescription);
+makeMarker(Locations.IKB_LIBRARY.position, Categories.STUDYING.icon, studyDescription);
+makeMarker(Locations.MACINNES_FIELD.position, Categories.EXERCISING.icon, exerciseDescription);
